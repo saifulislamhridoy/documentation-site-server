3f7f693780492bf7f55ca3b1d71fc264bfc6d804
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-08-08 17:56:13 +0600

    collect user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config()
 const app = express()
@@ -12,10 +13,46 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gns3pe2.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req,res,next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        res.status(401).send({message:'UnAuthorized Access'})
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+        if(err){
+            res.status(403).send({message:'Forbidden Access'})
+        }
+        req.decoded = decoded
+        next()
+      });
+}
+
 async function run() {
     try {
         await client.connect()
         const userCollection = client.db('documentation').collection('users')
+        const tutorialCollection = client.db('documentation').collection('tutorials')
+
+
+        app.put('/user/:email',async(req,res)=>{
+            const email = req.params.email;
+            const user = req.body
+            const filter = {email:email}
+            const option = {upsert:true}
+            const updateDoc ={
+                $set:user
+            }
+            const token = jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1d'})
+            const result = await userCollection.updateOne(filter,updateDoc,option)
+            res.send({result,token})
+        });
+      app.get('/tutorial',async(req,res)=>{
+        const query={}
+        const cursor = tutorialCollection.find(query)
+        const tutorial = await cursor.toArray()
+        res.send(tutorial)
+      })
     }
     finally {
 
